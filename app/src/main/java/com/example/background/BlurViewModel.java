d893f3f4ad94e22197b24edabec3bbbb731c299e
Yordy <kevinmacmasel@gmail.com>
2021-06-21 20:47:59 +0700

    first

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -18,16 +18,45 @@ package com.example.background;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkContinuation;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.example.background.workers.BlurWorker;
+import com.example.background.workers.CleanupWorker;
+import com.example.background.workers.SaveImageToFileWorker;
+
+import java.util.List;
+
+import static com.example.background.Constants.IMAGE_MANIPULATION_WORK_NAME;
+import static com.example.background.Constants.KEY_IMAGE_URI;
+import static com.example.background.Constants.TAG_OUTPUT;
+
 public class BlurViewModel extends AndroidViewModel {
 
+
     private Uri mImageUri;
+    private WorkManager mWorkManager;
+    private LiveData<List<WorkInfo>> mSavedWorkInfo;
+    private Uri mOutputUri;
+
+
 
     public BlurViewModel(@NonNull Application application) {
         super(application);
+
+        mWorkManager = WorkManager.getInstance(application);
+        mSavedWorkInfo = mWorkManager.getWorkInfosByTagLiveData(TAG_OUTPUT);
+
     }
 
     /**
@@ -35,6 +64,47 @@ public class BlurViewModel extends AndroidViewModel {
      * @param blurLevel The amount to blur the image
      */
     void applyBlur(int blurLevel) {
+        // Add WorkRequest to Cleanup temporary images
+//        WorkContinuation continuation =
+//                mWorkManager.beginWith(OneTimeWorkRequest.from(CleanupWorker.class));
+        WorkContinuation continuation = mWorkManager
+                .beginUniqueWork(IMAGE_MANIPULATION_WORK_NAME,
+                        ExistingWorkPolicy.REPLACE,
+                        OneTimeWorkRequest.from(CleanupWorker.class));
+
+        // Add WorkRequests to blur the image the number of times requested
+        for (int i = 0; i < blurLevel; i++) {
+            OneTimeWorkRequest.Builder blurBuilder =
+                    new OneTimeWorkRequest.Builder(BlurWorker.class);
+
+            // Input the Uri if this is the first blur operation
+            // After the first blur operation the input will be the output of previous
+            // blur operations.
+            if ( i == 0 ) {
+                blurBuilder.setInputData(createInputDataForUri());
+            }
+
+            continuation = continuation.then(blurBuilder.build());
+        }
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+
+
+        // Add WorkRequest to save the image to the filesystem
+        OneTimeWorkRequest save = new OneTimeWorkRequest.Builder(SaveImageToFileWorker.class)
+                .setConstraints(constraints) // This adds the Constraints
+                .addTag(TAG_OUTPUT)
+                .build();
+        continuation = continuation.then(save);
+
+        // Actually start the work
+        continuation.enqueue();
+
+
+
+
 
     }
 
@@ -43,6 +113,17 @@ public class BlurViewModel extends AndroidViewModel {
             return Uri.parse(uriString);
         }
         return null;
+    }
+    /**
+     * Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private Data createInputDataForUri(){
+        Data.Builder builder = new Data.Builder();
+        if(mImageUri != null){
+            builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+        }
+        return builder.build();
     }
 
     /**
@@ -57,6 +138,17 @@ public class BlurViewModel extends AndroidViewModel {
      */
     Uri getImageUri() {
         return mImageUri;
+    }
+
+    LiveData<List<WorkInfo>> getOutputWorkInfo() { return mSavedWorkInfo; }
+
+    void setOutputUri(String outputImageUri) {
+        mOutputUri = uriOrNull(outputImageUri);
+    }
+    Uri getOutputUri() { return mOutputUri; }
+
+    void cancelWork() {
+        mWorkManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME);
     }
 
 }
